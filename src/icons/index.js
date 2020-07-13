efcae4cc4e97664ec5c25ca08cb5fae2e399406e
deinyefa <df.eporwei@gmail.com>
2020-07-12 22:17:30 -0400

    format icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,14 @@
 import IconConfused from "./icon-confused";
+import IconDiamond from "./icon-diamond";
 import IconFacebook from "./icon-facebook";
 import IconHappy from "./icon-happy";
 import IconInstagram from "./icon-instagram";
+import IconPiggyBank from "./icon-piggy-bank";
 import IconSad from "./icon-sad-1";
 import IconVerySad from "./icon-sad-2";
+import IconSalad from "./icon-salad";
+import IconShoppingBag from "./icon-shopping-bag";
+import IconShoppingBagTime from "./icon-shopping-bag-time";
 import IconSmile from "./icon-smile";
 import IconTwitter from "./icon-twitter";
 
@@ -16,4 +21,9 @@ export {
   IconFacebook,
   IconInstagram,
   IconTwitter,
+  IconDiamond,
+  IconPiggyBank,
+  IconSalad,
+  IconShoppingBag,
+  IconShoppingBagTime,
 };
